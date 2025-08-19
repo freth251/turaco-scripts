5880fcb9c47c0a706a0ab3afe4975aa5fa9de94b
Amen Abshir <amentamerat@gmail.com>
2025-08-18 23:08:56 -0400

    fix contact us test failing 2

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 
 def new_session():
     options = Options()
+    options.binary_location = "/usr/bin/chromium-browser"
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
